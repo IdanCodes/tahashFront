10b94236e395a2b79f867c9b78347b817370429d
SolHTML <daboysfromisrael@gmail.com>
2025-10-22 20:35:00 +0300

    rework header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import { useUserInfo } from "../context/UserContext";
 import logo from "./assets/ILSpeeddcubinglogo.png";
+import "./animations.css";
+
 
 function Header(): JSX.Element {
   const userInfo = useUserInfo();
@@ -13,17 +15,33 @@ function Header(): JSX.Element {
         <img
           src={logo}
           alt="ILCubers Logo"
-          className="w-[65px] transition-transform duration-400 ease-in-out group-hover:scale-110"
+          className="w-[65px] transition-transform duration-200 ease-in-out group-hover:scale-110"
         />
-        <h1 className="inline-block text-[clamp(1.5rem,2vw,2.5rem)] whitespace-nowrap text-blue-100 transition-all duration-400 ease-in-out select-none group-hover:tracking-[0.05em]">
+        <h1 className="inline-block text-[clamp(1.5rem,2vw,2.5rem)] tracking-[0.03em] whitespace-nowrap text-blue-100 transition-all duration-200 ease-in-out select-none group-hover:tracking-[0.05em]">
           ILCubers - Weekly Competition
         </h1>
       </div>
-      <nav className="flex items-center justify-between">
-        <div className="flex gap-5">
-          <NavbarButton to="/" text="Home" />
-          {userInfo.isLoggedIn ? <LoggedInButtons /> : <LoggedOutButtons />}
-        </div>
+
+      <nav className="pr-[2vw]">
+        <ul className="flex list-none gap-5 text-center font-extrabold text-blue-100">
+          <li>
+            <NavbarButton to="/" text="Home" />
+          </li>
+          {userInfo.isLoggedIn ? (
+            <>
+              <li>
+                <NavbarButton to="/profile" text="Profile" />
+              </li>
+              <li>
+                <NavbarButton to="/scrambles" text="Scrambles" />
+              </li>
+            </>
+          ) : (
+            <li>
+              <NavbarButton to="/login" text="Login" />
+            </li>
+          )}
+        </ul>
       </nav>
     </div>
   );
@@ -35,33 +53,15 @@ function NavbarButton({ to, text }: { to: string; text: string }): JSX.Element {
       to={to}
       className={({ isActive }) =>
         clsx(
-          "box-border rounded-xl border-3 border-black text-2xl transition-all duration-125 ease-out",
-          isActive && "scale-109 bg-gray-500",
-          !isActive && "bg-gray-200 hover:scale-105 hover:bg-gray-400",
+          "relative px-[10px] text-[clamp(1.5rem,1vw,1.9rem)] transition-transform duration-300 ease-in-out",
+          isActive
+            ? "scale-105 text-blue-100"
+            : "text-blue-100 hover:scale-110",
         )
       }
     >
-      <div className="box-border px-3 py-2">
-        <p className="text-center text-black">{text}</p>
-      </div>
+      <span className="underline-hover">{text}</span>
     </NavLink>
-  );
-}
-
-function LoggedInButtons(): JSX.Element {
-  return (
-    <>
-      <NavbarButton to="/profile" text="Profile" />
-      <NavbarButton to="/scrambles" text="Scrambles" />
-    </>
-  );
-}
-
-function LoggedOutButtons(): JSX.Element {
-  return (
-    <>
-      <NavbarButton to="/login" text="Login" />
-    </>
   );
 }
 
