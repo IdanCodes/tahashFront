7980a322784b6e66ee25d7e7dbf111d5ed5b5fa3
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2025-10-29 14:26:40 +0200

    Added abort controllers for apiUtils methods

--- frontend/src/utils/API/apiUtils.ts
+++ frontend/src/utils/API/apiUtils.ts
@@ -5,8 +5,21 @@ import {
   isApiResponse,
 } from "@shared/types/api-response";
 import { errorObject } from "@shared/interfaces/error-object";
+import { isAbortError } from "../errorUtils";
 
 const basePath = "/api";
+
+/**
+ * A global set to store all active AbortControllers
+ */
+const activeAbortControllers = new Set<AbortController>();
+
+export function abortAllActiveRequests() {
+  console.log(`Aborting ${activeAbortControllers.size} requests...`);
+  for (const controller of activeAbortControllers) controller.abort();
+
+  activeAbortControllers.clear();
+}
 
 /**
  * Send a GET request to the API
@@ -15,10 +28,14 @@ const basePath = "/api";
  * @returns A ApiResponse that matches the response from the server
  */
 export async function sendGetRequest(path: string, headers: HeadersInit = {}) {
+  const abortController = new AbortController();
+  activeAbortControllers.add(abortController);
+
   try {
     const res = await fetch(basePath + path, {
       credentials: "include",
       headers,
+      signal: abortController.signal,
     });
 
     if (!res.ok)
@@ -31,12 +48,18 @@ export async function sendGetRequest(path: string, headers: HeadersInit = {}) {
     if (!isApiResponse(apiRes)) return errorResponse("Invalid Server Response");
 
     return apiRes;
-  } catch (err) {
+  } catch (err: any) {
+    if (isAbortError(err)) {
+      console.log(`GET request to ${path} aborted`);
+      return new ApiResponse(ResponseCode.Error, "Request Aborted");
+    }
     console.error(`Network error (GET to ${path}):`, err);
     return new ApiResponse(
       ResponseCode.Error,
       errorObject(`Network Error (GET to ${path})`, err),
     );
+  } finally {
+    activeAbortControllers.delete(abortController);
   }
 }
 
@@ -51,6 +74,9 @@ export async function sendPostRequest(
   body: object,
   headers: HeadersInit = {},
 ): Promise<ApiResponse> {
+  const abortController = new AbortController();
+  activeAbortControllers.add(abortController);
+
   try {
     const res = await fetch(basePath + path, {
       method: "post",
@@ -60,6 +86,7 @@ export async function sendPostRequest(
       },
       credentials: "include",
       body: JSON.stringify(body),
+      signal: abortController.signal,
     });
 
     if (!res.ok)
@@ -73,7 +100,14 @@ export async function sendPostRequest(
 
     return apiRes;
   } catch (err) {
+    if (isAbortError(err)) {
+      console.log(`POST request to ${path} aborted`);
+      return new ApiResponse(ResponseCode.Error, "Request Aborted");
+    }
+
     console.error(`Network error (POST to ${path}):`, err);
     return errorResponse("Network Error");
+  } finally {
+    activeAbortControllers.delete(abortController);
   }
 }
