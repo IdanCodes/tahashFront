9e8f4a3c0b2244a54924bc4bfb3d44d428ad953b
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2025-11-14 11:43:47 +0200

    add sol to admin list 👍

--- backend/src/utils/admin-helpers.ts
+++ backend/src/utils/admin-helpers.ts
@@ -2,7 +2,12 @@ import { Request, Response } from "express";
 import { CookieNames } from "@shared/constants/cookie-names";
 import { COOKIE_CONFIG } from "../config/env";
 
-export const ADMINS_LIST = ["2019SAHA01", "2019KEHI01", "2022STON03"];
+export const ADMINS_LIST = [
+  "2019SAHA01",
+  "2019KEHI01",
+  "2022STON03",
+  "2025TRIV01",
+];
 
 export function isAdmin(wcaId: string) {
   return ADMINS_LIST.includes(wcaId);
