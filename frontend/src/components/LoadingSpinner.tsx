babf055e861a796e178ca8a20d647d18a2a1d644
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2026-09-16 13:10:49 +0300

    Revert to regular loading spinner instead of cube gif

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import CubeLoader from "./assets/cube loader.gif";
+// import CubeLoader from "../assets/cube_loader.gif";
+import CubeLoader from "../assets/cube_loader.gif";
 
 function LoadingSpinner() {
-  return (
-    <img
+  /**
+   * To use the cube loader gif, use this img instead of the div:
+   * <img
       src={CubeLoader}
       alt="Loading..."
       className="mx-auto my-4 h-40 w-auto object-contain"
       style={{ imageRendering: "crisp-edges" }} //
+      role="status"
+    />
+   * 
+   */
+  return (
+     <div
+      className="mx-auto my-4 size-20 animate-spin rounded-full border-7 border-blue-500 border-t-transparent"
       role="status"
     />
   );
